c2628cce13163152383f76bf8032e01cb8223347
aiden.seo <aiden.seo@3i.ai>
2021-10-28 15:36:59 +0900

    init

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     // let v: Vec<i32> = Vec::new();
     // let v = vec![1,2,3];
@@ -71,4 +73,94 @@ fn main() {
     // let hello = String::from("Olá");
     // let hello = String::from("Здравствуйте");
     // let hello = String::from("Hola");
+    
+    let mut s = String::from("foo");
+
+    let s2 = "bar";
+
+    s.push_str(s2);
+
+    println!("s2: {}, s: {}", s2, s);
+
+    let mut s = String::from("lo");
+
+    s.push('l');
+
+    println!("s: {}", s);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1  + &s2;
+    println!("s3: {}, s2: {}", s3, s2);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    println!("s: {}, s1: {}, s2: {}, s3: {}",s, s1, s2, s3);
+
+    let hello = "Здравствуйте";
+    let answer = &hello[0..4];
+
+    println!("answer: {}", answer); 
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("HashMap: {:?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.into_iter()).collect();
+    println!("HashMap: {:?}", scores);
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    println!("Map: {:?}", map);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    println!("Score: {:?}", score);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("HashMap: {:?}", scores);
+    
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("Map: {:?}", map);
 }
