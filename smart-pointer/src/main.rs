4bcd0f1c4f05d663a4ee333096afc129442dcea5
coseo12 <coseo1221@gmail.com>
2021-11-23 10:32:32 +0900

    init

--- smart-pointer/src/main.rs
+++ smart-pointer/src/main.rs
@@ -1,10 +1,10 @@
-#[derive(Debug)]
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
+// #[derive(Debug)]
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
 
-use crate::List::{Cons, Nil};
+// use crate::List::{Cons, Nil};
 
 struct MyBox<T>(T);
 
@@ -39,11 +39,28 @@ impl Drop for CustomSmartPointer {
 }
 
 use std::mem::drop;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// #[derive(Debug)]
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
-    println!("{:?}", list);
+    // println!("{:?}", list);
 
     let x = 5;
     // let y = &x;
@@ -65,4 +82,30 @@ fn main() {
     };
 
     println!("CustomSmartPointer created: {}",  d.data);
+
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("Count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("Count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("Count after creating c = {} {:?}", Rc::strong_count(&a), c);
+    // }
+    // println!("Count after c goes out of scope = {}", Rc::strong_count(&a));
+
+
+    // println!("{:?} {:?}" , a, b);
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
