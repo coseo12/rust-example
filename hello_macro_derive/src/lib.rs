287948d43fd1f5a7fe560c7f7d9de3d0fec9245f
coseo12 <coseo1221@gmail.com>
2021-11-25 08:12:32 +0900

    init

--- hello_macro_derive/src/lib.rs
+++ hello_macro_derive/src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn;
 
-#[proc_macro_derive(HelloMacro)]
+// #[proc_macro_derive(HelloMacro)]
 pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
 
